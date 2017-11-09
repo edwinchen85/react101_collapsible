956101a1c5249a3d33bc4ec7c6bab9106d86e818
Edwin Chen <edwinchen85@gmail.com>
2017-11-09 20:42:42 +0800

    Panel will toggle 'is-expanded' class whenever user clicks on the panel

--- src/js/Collapsible.js
+++ src/js/Collapsible.js
@@ -4,12 +4,23 @@ import PropTypes from 'prop-types';
 class Collapsible extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isExpanded: false
+    }
+  }
+
+  handleToggle(e) {
+    e.preventDefault();
+    this.setState({
+      isExpanded: !this.state.isExpanded
+    });
   }
 
   render() {
     const {title, children} = this.props;
+    const {isExpanded} = this.state;
     return (
-      <div className="panel">
+      <div className={`panel ${isExpanded ? 'is-expanded' : ''}`} onClick={(e) => this.handleToggle(e)}>
         <div className="panel-heading">
           <h2>{title}</h2>
         </div>
