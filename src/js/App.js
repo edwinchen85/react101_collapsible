43b0887e228239b3ecfde4d631de49d39216469f
Edwin Chen <edwinchen85@gmail.com>
2017-11-09 20:47:34 +0800

    Add more content for first panel to demonstrate content is cut off beyond 200px

--- src/js/App.js
+++ src/js/App.js
@@ -14,6 +14,7 @@ class App extends React.Component {
                     <div className="panel-group">
                       <Collapsible title="Overview">
                         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione numquam magnam quo maiores ipsa. Eius.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga officiis, expedita possimus necessitatibus itaque quod perferendis voluptatem, odit incidunt voluptatibus iste unde cum eum, repudiandae consectetur in recusandae veritatis reprehenderit at harum nostrum natus ratione nam! Amet illum quo corporis asperiores dolorum nostrum mollitia, similique ab dolor tempora consequatur, veniam labore quas aut nesciunt iure saepe sunt fugiat quam minus! Possimus ipsam dolore pariatur facere quas? Dolore, in nobis? Sit repellendus porro illo nam sapiente similique impedit molestias obcaecati, illum quas soluta, atque maiores officia explicabo, fugiat sunt voluptates fugit nulla neque. Iste ipsa expedita, explicabo ipsum eum doloribus corporis.</p>
                       </Collapsible>
                       <Collapsible title="Features">
                         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione numquam magnam quo maiores ipsa. Eius.</p>
